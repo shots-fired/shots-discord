7a3671bfe11480ed61d82631e366782ab927af05
Jacob Hackett <jacobrhackett@gmail.com>
2018-09-19 20:18:48 -0400

    add a basic discord bot impl

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/shots-fired/shots-discord
+
+require (
+	github.com/bwmarrin/discordgo v0.18.0
+	github.com/gorilla/websocket v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/bwmarrin/discordgo"
+)
+
+var botID string
+
+const commandPrefix string = "!"
+
+func main() {
+	discord, err := discordgo.New("Bot " + os.Getenv("BOT_KEY"))
+	if err != nil {
+		panic(err)
+	}
+
+	user, err := discord.User("@me")
+	if err != nil {
+		panic(err)
+	}
+
+	botID = user.ID
+
+	discord.AddHandler(commandHandler)
+
+	discord.AddHandler(func(discord *discordgo.Session, ready *discordgo.Ready) {
+		err = discord.UpdateStatus(0, "I am Shots! Hear me roar!")
+		if err != nil {
+			fmt.Println("Error attempting to set my status")
+		}
+		fmt.Printf("Shots has started on %d servers", len(discord.State.Guilds))
+	})
+
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer discord.Close()
+
+	<-make(chan struct{})
+}
+
+func commandHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	user := message.Author
+	if user.ID == botID || user.Bot {
+		//Do nothing because the bot is talking
+		return
+	}
+
+	fmt.Printf("Message: %+v || From: %s\n", message.Message, message.Author)
 }
