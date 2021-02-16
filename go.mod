62b76dbb61130b4b9308547b6ea25528c57facf1
Ryan Wiesenberg <rmwiesenberg@users.noreply.github.com>
2021-02-16 03:08:29 -0500

    working support for verbing (#4)

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module github.com/shots-fired/shots-discord
 
-require (
-	github.com/bwmarrin/discordgo v0.18.0
-	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/shots-fired/shots-common v0.0.0-20180925012708-830c4055bfaf
-	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b // indirect
-)
-
 go 1.13
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/shots-fired/shots-common v0.1.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
--- main.go
+++ main.go
@@ -15,35 +15,6 @@ import (
 
 var botID string
 
-func main() {
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
-	if err != nil {
-		panic(err)
-	}
-
-	user, err := discord.User("@me")
-	if err != nil {
-		panic(err)
-	}
-
-	botID = user.ID
-
-	discord.AddHandler(commandHandler)
-	discord.AddHandler(readyHandler)
-
-	err = discord.Open()
-	if err != nil {
-		panic(err)
-	}
-	defer discord.Close()
-
-	<-make(chan struct{})
-}
-
-func readyHandler(discord *discordgo.Session, ready *discordgo.Ready) {
-	fmt.Printf("Shots has started on %d servers", len(discord.State.Guilds))
-}
-
 func registerTwitchHandler(discord *discordgo.Session, message *discordgo.MessageCreate, splitMessage []string) {
 	if len(splitMessage) > 1 {
 		discord.ChannelMessageSend(message.ChannelID, "Registered "+splitMessage[1])
@@ -89,13 +60,74 @@ func commandHandler(discord *discordgo.Session, message *discordgo.MessageCreate
 		discord.ChannelMessageSend(message.ChannelID, help)
 	}
 
+	splitMessage := strings.Split(message.Content, " ")
+	if lookupFunc, ok := lookup[splitMessage[0][1:]]; ok {
+		lookupFunc(discord, message, splitMessage[1:])
+	}
+}
+
+func mentionHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	baseURL := "https://www.dictionaryapi.com/api/v3/references/collegiate/json/%s?key=%s"
+	splitMessage := strings.Split(message.Content, " ")
+	words := []string{splitMessage[1] + "es", splitMessage[1] + "s"}
+	for _, word := range words {
+		res, err := http.Get(fmt.Sprintf(baseURL, word, os.Getenv("DICTIONARY_API_KEY")))
+		if err != nil {
+			panic(err)
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode == http.StatusOK {
+			bodyBytes, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+			if strings.Contains(bodyString, "\"fl\":\"verb\"") {
+				discord.ChannelMessageSend(message.ChannelID, fmt.Sprintf("_%s %s_", word, strings.Join(splitMessage[2:], " ")))
+				return
+			}
+		}
+	}
+	discord.ChannelMessageSend(message.ChannelID, "wat")
+}
+
+func messageHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
 	if message.Author.ID == botID || message.Author.Bot {
 		// Do nothing because the bot is talking
 		return
 	} else if strings.HasPrefix(message.Content, "!") {
-		splitMessage := strings.Split(message.Content, " ")
-		if lookupFunc, ok := lookup[splitMessage[0][1:]]; ok {
-			lookupFunc(discord, message, splitMessage[1:])
-		}
+		commandHandler(discord, message)
+	} else if strings.HasPrefix(message.Content, fmt.Sprintf("<@!%s> ", botID)) {
+		mentionHandler(discord, message)
 	}
+}
+
+func readyHandler(discord *discordgo.Session, ready *discordgo.Ready) {
+	fmt.Printf("Shots has started on %d servers", len(discord.State.Guilds))
+}
+
+func main() {
+	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	user, err := discord.User("@me")
+	if err != nil {
+		panic(err)
+	}
+
+	botID = user.ID
+
+	discord.AddHandler(messageHandler)
+	discord.AddHandler(readyHandler)
+
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer discord.Close()
+
+	<-make(chan struct{})
 }
